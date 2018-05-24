aae969795de4302ed76c09fbd5b671fb0d8940b1
Krish <mailkimu@gmail.com>
2018-05-24 19:33:44 +0530

    Added firebase config file

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAxkgvroOid7TT3AbUbNrhaIwEfi5wWz2Y",
+    authDomain: "tutionmanager-481c0.firebaseapp.com",
+    databaseURL: "https://tutionmanager-481c0.firebaseio.com",
+    projectId: "tutionmanager-481c0",
+    storageBucket: "tutionmanager-481c0.appspot.com",
+    messagingSenderId: "636820988207"
+}
